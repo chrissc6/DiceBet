ec18053aa40402be9be9d2807abce95ae51ff705
chris <christopherscaudill@gmail.com>
2019-02-09 00:08:51 -0500

    works ok

--- DiceBet/GameClass.cs
+++ DiceBet/GameClass.cs
@@ -8,104 +8,137 @@ namespace DiceBet
 {
     class GameClass
     {
+        int chips = 5;
 
-        int chips = 10;
-        int bonus = 0;
-        int point = 0;
-        int dice = 0;
-        int total;
+
 
         public void OtherGame()
+
         {
             Random rng = new Random();
-            Console.WriteLine($"start");
-            var answer = Console.ReadLine();
+            
+            int dice = 0;
+            int total = 0;
+            
+            Console.WriteLine($"You have {chips} chips. \nPress ENTER to roll. (99 to Quit)");
+            string answer = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine($"You have {chips} chips. Rolling...");
 
-            while (answer != "99")
+            while (answer != "99" || chips <= 0 || chips > 100)
             {
-                Console.WriteLine($"You have {chips} chips. Press ENTER to roll. (99 to Quit)");
-                answer = Console.ReadLine();
+                int current = dice;
+                string ans = "NA";
 
-                (bonus, dice) = Roll1(0);
-                if (bonus == 1)
+                total = Roll1();
+                if (total == 7 || total == 11)
                 {
-                    chips++;
-                    Console.WriteLine($"WINNER WINNER!!! You rolled a {dice} (Chips +1)");
-                    dice = 0;
-                    bonus = 0;
-                    continue;
+                    ans = "Win";
+                    
+                    
                 }
-                else if (bonus == -1)
+                else if (total == 2 || total == 3 || total == 12)
                 {
-                    chips--;
-                    Console.WriteLine($"Better luck next time. You rolled a {dice} (Chips -1)");
-                    dice = 0;
-                    bonus = 0;
-                    continue;
+                    ans = "Lose";
+                    
+                    
                 }
                 else
                 {
-                    point++;
-                    Console.WriteLine($"You rolled a {dice}, roll another {dice} to win");
-                    bonus = 0;
-                    dice = Roll2(dice);
+                        Console.WriteLine($"You rolled a {total}, roll another {total} to win. Roll a 7 and lose.");
+                        ans = Roll2(total);
                 }
 
                 
-
-
+                if(ans == "Win")
+                {
+                    Console.WriteLine($"WINNER WINNER!!! You rolled a {total} (Chips +1)");
+                    chips++;
+                    if(chips > 100)
+                    {
+                        Console.WriteLine("\nThanks for playing");
+                        Console.ReadKey();
+                        break;
+                    }
+                    total = 0;
+                    OtherGame();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"Better luck next time. You rolled a {total} (Chips -2)");
+                    chips = chips -2;
+                    if(chips <= 0)
+                    {
+                        Console.WriteLine("\nThanks for playing");
+                        Console.ReadKey();
+                        break;
+                    }
+                    total = 0;
+                    OtherGame();
+                    break;
+                }
+    
             }
+            Console.WriteLine("Thanks for playing");
+            return;
+            
 
+        }
+
+
+        private int Roll1()
+        {
+            
+            Random rng = new Random();
+            int die1 = rng.Next(1, 7);
+            int die2 = rng.Next(1, 7);
+            int total = die1 + die2;
+
+            return total;
             
         }
 
 
-        private (int, int) Roll1(int bonus)
+        private string Roll2(int current)
         {
+            int total2 = 0;
+            int total3 = 0;
             Random rng = new Random();
-            int die1 = rng.Next(1, 7);
-            int die2 = rng.Next(1, 7);
-            bonus = 0;
-            point = 0;
-            total = die1 + die2;
 
-            if (total == 7 || total == 11)
+            while (current != total2)
             {
-                return (bonus++, total);
+                Console.WriteLine("Press ENTER to roll");
+
+                Console.ReadKey();
+                int die1 = 0;
+                int die2 = 0;
+                die1 = rng.Next(1, 7);
+                die2 = rng.Next(1, 7);
+                total2 = die1 + die2;
+                int die3 = 0;
+                int die4 = 0;
+                die3 = rng.Next(1, 7);
+                die4 = rng.Next(1, 7);
+                total3 = die3 + die4;
+
+                total2 = (total2 + total3) / 2;
+
+                if (total2 != 7)
+                {
+                    Console.WriteLine($"You rolled a ....{total2}");
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("You rolled a 7 too late");
+                    return "Lose";
+                }
             }
-            else if(total == 2 || total == 3 || total == 12)
-            {
-                return (bonus--, total);
-            }
-            else
-            {
-                return (bonus = 0, total);
-            }
+            
+            Console.WriteLine("You got it!");
+            return "Win";
         }
 
-
-        private int Roll2(int dice)
-        {
-            Random rng = new Random();
-            int die1 = rng.Next(1, 7);
-            int die2 = rng.Next(1, 7);
-            bonus = 0;
-            point = 0;
-            //int rdice = dice;
-            total = die1 + die2;
-
-            if (total == dice)
-            {
-                return bonus++;
-            }
-            else if (total != 7)
-            {
-                ;
-            }
-            else
-            {
-                return bonus--;
-            }
-        }
     }
 }
